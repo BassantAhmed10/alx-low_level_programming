818d4831bbcff482dc78afcbd7d6553695ee32f4
BassantAhmed10 <ba4415151@gmail.com>
2023-07-13 17:24:06 -0700

    My first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf('with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
